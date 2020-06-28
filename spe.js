a94013d975c6d5cf9e72b772b4b953bb14cdaa56
MGBB <magebolgmail.com>
2020-06-28 02:20:17 -0500

    2nd

--- spe.js
+++ spe.js
@@ -4,21 +4,21 @@
 $(document).on('knack-scene-render.scene_286', function(event, scene) {
 	var apid = appid;
 	var apk = apikey;
-  
+
   $("#kn-scene_286").parent().parent().css('height','100%');
   $("#kn-scene_286").parent().css('height','100%');
   $("#kn-scene_286").css('height','100%');
   $("#kn-scene_286").append('<div id="canvas"></div>');
   $("#canvas").after('<button id="save-button">Salvar Diagrama</button>');
- 
+
     LazyLoad.js(['https://unpkg.com/bpmn-js@7.0.1/dist/bpmn-modeler.development.js','https://unpkg.com/jquery@3.3.1/dist/jquery.js'], function () {
         console.log('All my files have completed loading!');
- 
+
 	var css_files = ['https://unpkg.com/bpmn-js@7.0.1/dist/assets/diagram-js.css', 'https://unpkg.com/bpmn-js@7.0.1/dist/assets/bpmn-font/css/bpmn.css'];
 	LazyLoad.css(css_files, function () {
            console.log('All my CSS files have completed loading!');
-	}); 
-      
+	});
+
   /*** Viewer or Modeler Instances ***/
     // modeler instance
 	var bpmnModeler = new BpmnJS({
@@ -28,10 +28,10 @@ $(document).on('knack-scene-render.scene_286', function(event, scene) {
         }
 	});
   /***********************************************************/
-  
+
   /*** CREATE new diagram */
 	const start = async function () {
-            
+
 	//async function openD(bpmnXML) {
     try {
 
@@ -48,43 +48,43 @@ $(document).on('knack-scene-render.scene_286', function(event, scene) {
 
     } catch (err) {
           console.log(err.message, err.warnings);
-      } 
+      }
 	}
 	// Call start
 	start();
   /***********************************************************/
-  
+
   /*** Save diagram contents and print them to the console. ***/
 	async function exportDiagram() {
         try {
           var result = await bpmnModeler.saveXML({ format: true });
           console.log('DIAGRAM', result.xml);
-                     
+
             var stringNew = result.xml;
             var stringNew2 = 'test test';
             var filename = 'process.txt';
-          
+
             var formData = new FormData();
             var archivo = new Blob([stringNew2], {type: 'text/plain'}) ;
             formData.append('files', archivo, filename);
-          
+
 			sendFile(formData, "object_33", apid, apk);
-           
+
         } catch (err) {
           console.error('No pudo salvarse el diagram BPMN 2.0', err);
         }
 	}
   /***********************************************************/
-  
+
   // wire save button
       $('#save-button').click(exportDiagram);
   });
-}); 
+});
 
 //-------------------------------------------   VER  -----------------------------------------------------------------
 //----------------------------------------------------------------------------------------------------------------------
 $(document).on('knack-scene-render.scene_285', function(event, scene) {
-  
+
   $("#kn-scene_285").parent().parent().css('height','100%');
   $("#kn-scene_285").parent().css('height','100%');
   $("#kn-scene_285").css('height','100%');
@@ -92,12 +92,12 @@ $(document).on('knack-scene-render.scene_285', function(event, scene) {
 
  LazyLoad.js(['https://unpkg.com/bpmn-js@7.0.1/dist/bpmn-viewer.development.js','https://unpkg.com/jquery@3.3.1/dist/jquery.js'], function () {
         console.log('All my files have completed loading!');
- 
+
 		var css_files = ['https://unpkg.com/bpmn-js@7.0.1/dist/assets/diagram-js.css', 'https://unpkg.com/bpmn-js@7.0.1/dist/assets/bpmn-font/css/bpmn.css'];
 		LazyLoad.css(css_files, function () {
              console.log('All my CSS files have completed loading!');
 		});
-  
+
 		var diagramUrl = 'https://cdn.staticaly.com/gh/bpmn-io/bpmn-js-examples/dfceecba/starter/diagram.bpmn';
 
   /*** Viewer or Modeler Instances ***/
@@ -120,39 +120,39 @@ $(document).on('knack-scene-render.scene_285', function(event, scene) {
 
           // zoom to fit full viewport
           canvas.zoom('fit-viewport');
-          
+
         } catch (err) {
           console.error('could not import BPMN 2.0 diagram', err);
         }
       }
 
       // load external diagram file via AJAX and open it
-      	$.get(diagramUrl, function(data) {;   
+      	$.get(diagramUrl, function(data) {;
              openDiagram(data);
 		});
   /***********************************************************/
   });
-}); 
+});
 
 
 //-------------------------------------------   EDITAR  ----------------------------------------------------------------
 //----------------------------------------------------------------------------------------------------------------------
 $(document).on('knack-scene-render.scene_284', function(event, scene) {
-  
+
   $("#kn-scene_284").parent().parent().css('height','100%');
   $("#kn-scene_284").parent().css('height','100%');
   $("#kn-scene_284").css('height','100%');
   $("#kn-scene_284").append('<div id="canvas"></div>');
   $("#canvas").after('<button id="save-button">Salvar Diagrama</button>');
-  
+
  LazyLoad.js(['https://unpkg.com/bpmn-js@7.0.1/dist/bpmn-modeler.development.js','https://unpkg.com/jquery@3.3.1/dist/jquery.js'], function () {
         console.log('All my files have completed loading!');
- 
+
 	var css_files = ['https://unpkg.com/bpmn-js@7.0.1/dist/assets/diagram-js.css', 'https://unpkg.com/bpmn-js@7.0.1/dist/assets/bpmn-font/css/bpmn.css'];
 	LazyLoad.css(css_files, function () {
            console.log('All my CSS files have completed loading!');
 	});
-  
+
 	var diagramUrl = 'https://cdn.staticaly.com/gh/bpmn-io/bpmn-js-examples/dfceecba/starter/diagram.bpmn';
 
   /*** Viewer or Modeler Instances ***/
@@ -164,7 +164,7 @@ $(document).on('knack-scene-render.scene_284', function(event, scene) {
         }
 	});
   /***********************************************************/
-  
+
   /*** VIEW existing diagram */
       async function openDiagram(bpmnXML) {
 
@@ -178,18 +178,18 @@ $(document).on('knack-scene-render.scene_284', function(event, scene) {
 
           // zoom to fit full viewport
           canvas.zoom('fit-viewport');
-          
+
         } catch (err) {
           console.error('could not import BPMN 2.0 diagram', err);
         }
       }
 
       // load external diagram file via AJAX and open it
-      	$.get(diagramUrl, function(data) {;   
+      	$.get(diagramUrl, function(data) {;
              openDiagram(data);
 		});
   /***********************************************************/
-   
+
   /*** Save diagram contents and print them to the console. ***/
 	async function exportDiagram() {
         try {
@@ -203,12 +203,12 @@ $(document).on('knack-scene-render.scene_284', function(event, scene) {
           console.error('No pudo salvarse el diagram BPMN 2.0', err);
         }
 	}
-  /***********************************************************/  
-  
+  /***********************************************************/
+
   // wire save button
       $('#save-button').click(exportDiagram);
   });
-}); 
+});
 
 
 
@@ -258,13 +258,13 @@ $(document).ready(function () {
 
 
 var addGroupExpandCollapse1 = function(view) {
-  
+
 $('#' + view.key + ' .kn-group-level-1').nextUntil('.kn-group-level-1').toggle();
-  
+
   $('#' + view.key + ' table thead tr th span a').attr('href','');
-  
+
   $('#' + view.key + ' .kn-table-group').css("cursor","pointer");
-  
+
   $('#' + view.key + " .kn-group-level-1 td").each(function () {
     if($(this).text().length > 1) {
       var RowText = $(this).html();
@@ -275,7 +275,7 @@ $('#' + view.key + ' .kn-group-level-1').nextUntil('.kn-group-level-1').toggle()
 
   $('#' + view.key + ' .kn-group-level-1').click(function() {
     $(this).nextUntil('.kn-group-level-1').toggle();
-    
+
     if($(this).html().indexOf('fa-minus') !== -1) {
       $(this).html($(this).html().replace('minus', 'plus'));
     } else {
@@ -291,7 +291,7 @@ function sendData(data, object, appid, apikey) {
                               type: 'POST',
                               headers: {
                               "Content-Type": "application/json",
-                              "X-Knack-Application-Id":appid, 
+                              "X-Knack-Application-Id":appid,
                               "X-Knack-REST-API-Key":apikey
 								},
                               data: JSON.stringify(data),
@@ -313,7 +313,7 @@ function sendFile(form, object, app_id, apikey) {
       url:  url,
       type: 'POST',
       headers: {
-			"X-Knack-Application-Id":app_id, 
+			"X-Knack-Application-Id":app_id,
 			"X-Knack-REST-API-Key":apikey
 			},
       processData: false,
@@ -329,13 +329,14 @@ function sendFile(form, object, app_id, apikey) {
             console.log(datos);
             //sendData(datos, object, app_id, apikey);
 				},
-      error: function(XMLHttpRequest, textStatus, errorThrown) { 
+      error: function(XMLHttpRequest, textStatus, errorThrown) {
          console.log("Status: " + textStatus);
-         console.log("Error: " + errorThrown); 
-				}  
+         console.log("Error: " + errorThrown);
+				}
     }).done(function(responseData) {
-			Knack.hideSpinner();        
+			Knack.hideSpinner();
     });
 }
+console.log(process.env);
 var appid = process.env.APP_ID;
 var apikey = process.env.API_KEY;
